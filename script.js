f925aabb935c5cb21ecc487c57821ffdaef4161b
rey-1e <kanaderaj1216@gmail.com>
2026-04-05 18:58:57 +0530

    added playRound function to derive winner and update scores from computerchoice and humanchoice

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ function getHumanChoice() {
     return ans; 
 }
 
+let humanScore = 0; 
+let computerScore = 0; 
+
+function playRound(computerChoice, humanChoice) {
+
+    if(computerChoice == "rock" && humanChoice == "scissors") {
+        console.log("Computer Wins!");
+        computerScore++;
+    } else if(computerChoice == "rock" && humanChoice == "paper") {
+        console.log("Human Wins!");
+        humanScore++;
+    } else if(computerChoice == "paper" && humanChoice == "rock") {
+        console.log("Computer Wins!");
+        computerChoice++;
+    } else if(computerChoice == "paper" && humanChoice == "scissors") {
+        console.log("Human Wins!");
+        humanScore++;
+    } else if(computerChoice == "scissors" && humanChoice == "rock") {
+        console.log("Human Wins!");
+        humanScore++;
+    } else if(computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("Computer Wins!");
+        computerScore++;
+    } else {
+        console.log("It's a Tie!");
+    }
+
+}
+
