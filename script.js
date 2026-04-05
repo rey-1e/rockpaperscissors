f2258f7b4f69f3c5bd01077422f8c6130e223c21
rey-1e <kanaderaj1216@gmail.com>
2026-04-05 18:37:58 +0530

    wrote getComputerChoice function() to drive out computer's choice amongst rock, paper, scissors

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    let num = Math.random();
+
+    if(num <= 0.33) {
+        return "rock";
+    } else if(num <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
