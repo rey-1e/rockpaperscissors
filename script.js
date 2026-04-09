3375f7d4fdb1e0ba1bd4f1729ab304182de36d44
rey-1e <kanaderaj1216@gmail.com>
2026-04-09 23:39:06 +0530

    added UI

--- script.js
+++ script.js
@@ -10,62 +10,118 @@ function getComputerChoice() {
     } else {
         return "scissors";
     }
-}
-
-function getHumanChoice() {
-    let ans = prompt("Choose amongst (ROCK, PAPER, SCISSORS)", "VALUE");
-    ans = ans.toLowerCase(ans);
-    
-    return ans; 
-}
+};
 
 let humanScore = 0; 
 let computerScore = 0; 
 
+//storing ids of human score and computer score; 
+let human = document.querySelector("#humanScore");
+let computer = document.querySelector("#computerScore");
+
+//taking second score container; 
+let humanPick = document.querySelector("#humanPick");
+let computerPick = document.querySelector("#computerPick");
+
+
+
+//winning message heading: 
+let element = document.createElement("h1");
+element.style.textAlign = "center";
+
+//store the header; 
+let header = document.querySelector("header");
+
+
+//event listeners for all three buttons; 
+let btns = document.querySelectorAll("button");
+
+btns[0].addEventListener("click", () => {
+    //rock is pressed; 
+    if(humanScore >= 5 || computerScore >= 5) {
+        humanScore = 0; 
+        computerScore = 0; 
+        element.remove();
+    }
+    let computerHaha = getComputerChoice();
+    computerPick.innerText = computerHaha;
+    humanPick.innerText = "rock";
+
+    playGame(computerHaha, "rock");
+});
+btns[1].addEventListener("click", () => {
+    //paper is pressed; 
+
+    if(humanScore >= 5 || computerScore >= 5) {
+        humanScore = 0; 
+        computerScore = 0; 
+        element.remove();
+    }
+    let computerHaha = getComputerChoice();
+    computerPick.innerText = computerHaha;
+    humanPick.innerText = "paper";
+
+    playGame(computerHaha, "paper");
+});
+btns[2].addEventListener("click", () => {
+    //scissors is pressed; 
+
+    if(humanScore >= 5 || computerScore >= 5) {
+        humanScore = 0; 
+        computerScore = 0; 
+        element.remove();
+    }
+    let computerHaha = getComputerChoice();
+    computerPick.innerText = computerHaha;
+    humanPick.innerText = "scissors";
+
+    playGame(computerHaha, "scissors");
+})
+
+
 function playRound(computerChoice, humanChoice) {
 
     if(computerChoice == "rock" && humanChoice == "scissors") {
-        console.log("Computer Wins!");
+        
         computerScore++;
     } else if(computerChoice == "rock" && humanChoice == "paper") {
-        console.log("Human Wins!");
+        
         humanScore++;
     } else if(computerChoice == "paper" && humanChoice == "rock") {
-        console.log("Computer Wins!");
-        computerChoice++;
+        
+        computerScore++;
     } else if(computerChoice == "paper" && humanChoice == "scissors") {
-        console.log("Human Wins!");
+        
         humanScore++;
     } else if(computerChoice == "scissors" && humanChoice == "rock") {
-        console.log("Human Wins!");
+        
         humanScore++;
     } else if(computerChoice == "scissors" && humanChoice == "paper") {
-        console.log("Computer Wins!");
+        
         computerScore++;
-    } else {
-        console.log("It's a Tie!");
     }
 
 }
 
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        let currHumanChoice = getHumanChoice();
-        let currComputerChoice = getComputerChoice();
 
-        playRound(currComputerChoice, currHumanChoice);
-        console.log(`Human : ${humanScore}` + " " + `Computer : ${computerScore}`);
-    }
+function playGame(currComputerChoice, currHumanChoice) {
+
+    playRound(currComputerChoice, currHumanChoice);
+    human.innerText = humanScore;
+    computer.innerText = computerScore;
 
     //final winner : 
+    if(humanScore >= 5 || computerScore >= 5) {
     if(humanScore > computerScore) {
-        console.log("Human Wins the Battle");
+        element.textContent = "Human Wins the Battle";
+        header.after(element);
+        
     } else if(humanScore < computerScore) {
-        console.log("Computer Wins the Battle");
-    } else {
-        console.log("There's no Winner to this Match");
+        element.textContent = "Computer Wins the Battle";
+        header.after(element);
+
+    }
+
     }
 }
 
-
-playGame();
